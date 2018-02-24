a89a0cfa7faef9d7abba7bdd61f5175857954514
Dimitri Merejkowsky <d.merej@gmail.com>
2018-02-24 23:37:03 +0100

    ExampleTest -> ChuckNorrisInstrumentedTest

--- android/app/src/androidTest/java/com/chucknorris/ChuckNorrisInstrumentedTest.java
+++ android/app/src/androidTest/java/com/chucknorris/ChuckNorrisInstrumentedTest.java
@@ -9,11 +9,6 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
