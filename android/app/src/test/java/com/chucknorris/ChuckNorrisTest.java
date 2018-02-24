e797ce208b02847275eb2467c4bf54eecbc62ef2
Dimitri Merejkowsky <d.merej@gmail.com>
2018-02-24 22:51:10 +0100

    Add implem and tests for jna

--- android/app/src/test/java/com/chucknorris/ChuckNorrisTest.java
+++ android/app/src/test/java/com/chucknorris/ChuckNorrisTest.java
@@ -4,14 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ChuckNorrisTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testGetVersion() {
+        String version = ChuckNorrisLib.getVersion();
+        assertEquals(version, "0.1");
     }
 }
