58f11bef70bb5b60c29d97fff8eea6cd9fd0063d
Dimitri Merejkowsky <d.merej@gmail.com>
2018-02-25 01:26:13 +0100

    Android app done !

--- android/app/src/main/java/com/chucknorris/MainActivity.java
+++ android/app/src/main/java/com/chucknorris/MainActivity.java
@@ -1,13 +1,27 @@
 package com.chucknorris;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ChuckNorrisLib cklib = new ChuckNorrisLib();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        final TextView view = (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = cklib.getFact();
+                view.setText(fact);
+            }
+        });
     }
 }
