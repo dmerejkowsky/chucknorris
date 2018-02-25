5d015eff02365940701ae7f128beef6d8de70405
Dimitri Merejkowsky <d.merej@gmail.com>
2018-02-25 01:07:56 +0100

    Building chucknorris shared

--- cpp/android-build.sh
+++ cpp/android-build.sh
@@ -5,5 +5,5 @@ build_folder=build/android/x86_64/
 
 rm -rf $build_folder
 mkdir -p $build_folder
-conan install . --profile android-x86_64 --install-folder $build_folder --build missing
+conan install . --profile android-x86_64  --options ChuckNorris:shared=True --install-folder $build_folder --build missing
 conan build . --build-folder $build_folder
