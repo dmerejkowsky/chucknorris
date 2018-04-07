a913223df7b81573f398b4bc7c102a5242cacd9e
Dimitri Merejkowsky <d.merej@gmail.com>
2018-04-07 17:08:31 +0200

    chucknorris.py: define a real class

--- python/chucknorris.py
+++ python/chucknorris.py
@@ -1,11 +1,25 @@
 from _chucknorris import lib, ffi
 
-def c_to_str(c_data):
-    return ffi.string(c_data).decode()
 
-c_version = lib.chuck_norris_version()
-print(c_to_str(c_version))
-chuck_norris = lib.chuck_norris_init()
-c_fact = lib.chuck_norris_get_fact(chuck_norris)
-print(c_to_str(c_fact))
-lib.chuck_norris_deinit(chuck_norris)
+class ChuckNorris:
+    def __init__(self):
+        self._ck = lib.chuck_norris_init()
+
+    def get_fact(self):
+        c_fact = lib.chuck_norris_get_fact(self._ck)
+        fact_as_bytes = ffi.string(c_fact)
+        return fact_as_bytes.decode("UTF-8")
+
+    def __del__(self):
+        lib.chuck_norris_deinit(self._ck)
+
+
+def main():
+    chuck_norris = ChuckNorris()
+    fact = chuck_norris.get_fact()
+    print(fact)
+
+
+if __name__ == "__main__":
+    main()
+
