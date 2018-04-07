45c6a6c07f33a694f0da2b85f3feff59b14ac35e
Dimitri Merejkowsky <d.merej@gmail.com>
2018-04-07 17:06:17 +0200

    C++: small fixes
    Use json generator
    Alaways set sqlite3:pic=True
    
    Fix C example

--- cpp/ChuckNorris/src/c_wrapper.cpp
+++ cpp/ChuckNorris/src/c_wrapper.cpp
@@ -13,14 +13,12 @@ chuck_norris_t* chuck_norris_init()
   return reinterpret_cast<chuck_norris*>(ck);
 }
 
-const char* chuck_norris_get_fact(chuck_norris_t* chuck_norris)
+char* chuck_norris_get_fact(chuck_norris_t* chuck_norris)
 {
   auto ck = reinterpret_cast<ChuckNorris*>(chuck_norris);
   std::string fact = ck->getFact();
-  const char* result = fact.c_str();
+  char* result = strdup(fact.c_str());
   return result;
-  //const char* result = strdup(fact.c_str());
-  //return result;
 }
 
 void chuck_norris_deinit(chuck_norris_t* chuck_norris)
--- cpp/ChuckNorris/src/main.c
+++ cpp/ChuckNorris/src/main.c
@@ -5,10 +5,10 @@
 
 int main()
 {
-  chuck_norris_t* ck;
-  const char* fact = chuck_norris_get_fact(ck);
+  chuck_norris_t* ck = chuck_norris_init();
+  char* fact = chuck_norris_get_fact(ck);
   printf("%s\n", fact);
-  /*free(fact);*/
+  free(fact);
   chuck_norris_deinit(ck);
   return 0;
 }
