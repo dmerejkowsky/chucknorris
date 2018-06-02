6c0c14b42c97ceb52ad22a8b17c3c258e904183a
Dimitri Merejkowsky <d.merej@gmail.com>
2018-06-02 18:26:31 +0200

    java: cleanup, fix whitespace

--- java/src/main/java/com/chucknorris/ChuckNorris.java
+++ java/src/main/java/com/chucknorris/ChuckNorris.java
@@ -1,10 +1,33 @@
 package com.chucknorris;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
 public class ChuckNorris {
-    public static void main(String[] args) {
-        ChuckNorrisLib lib = new ChuckNorrisLib();
-        String fact = lib.getFact();
-        System.out.println(fact);
-        lib.close();
-    }
+	private Pointer ckPointer;
+
+	private static CLibrary loadChuckNorrisLibrary() {
+		return (CLibrary) Native.loadLibrary("chucknorris", CLibrary.class);
+	}
+
+	public interface CLibrary extends Library {
+		CLibrary INSTANCE = loadChuckNorrisLibrary();
+
+        Pointer chuck_norris_init();
+		String chuck_norris_get_fact(Pointer pointer);
+		void chuck_norris_deinit(Pointer pointer);
+	}
+
+	public ChuckNorris() {
+		ckPointer = CLibrary.INSTANCE.chuck_norris_init();
+	}
+
+	public String getFact() {
+		return CLibrary.INSTANCE.chuck_norris_get_fact(ckPointer);
+	}
+
+	public void close() {
+		CLibrary.INSTANCE.chuck_norris_deinit(ckPointer);
+	}
 }
