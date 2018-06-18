d95a78793122128ae56a1ab7653f25c35f9909ac
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-06-18 15:44:48 +0200

    Fix chucknorris recipe

--- cpp/ChuckNorris/conanfile.py
+++ cpp/ChuckNorris/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class ChucknorrisConan(ConanFile):
     name = "ChuckNorris"
-    version = "0.1"
+    version = "0.2"
     license = "MIT"
     url = "https://github.com/dmerejkowsky/cpp-mobile-example"
     description = "Chuck Norris does not need a description"
@@ -13,17 +13,23 @@ class ChucknorrisConan(ConanFile):
     generators = "cmake", "json"
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
     requires = "sqlite3/3.21.0@dmerej/test"
+    keep_imports = True
 
     def build(self):
         cmake = CMake(self)
-        cmade_definitions = {}
+        cmake_definitions = {}
         if self.options.shared:
-            cmade_definitions["BUILD_SHARED_LIBS"] = "ON"
-        cmake.configure(defs=cmade_definitions)
+            cmake_definitions["BUILD_SHARED_LIBS"] = "ON"
+        cmake.configure(defs=cmake_definitions)
         cmake.build(args=["--", "-v"])
 
+    def imports(self):
+        self.copy("*libc++_shared.so", dst="lib", keep_path=False)
+
     def package(self):
-        self.copy("bin/cpp_demo", dst="bin")
+        self.copy("bin/cpp_demo", dst="bin", keep_path=False)
+        self.copy("lib/libchucknorris.so", dst="lib", keep_path=False)
+        self.copy("lib/libc++_shared.so", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["chucknorris"]
